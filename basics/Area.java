0f40c96147d43474361960661f55b3ef07cf6494
soumya780 <soumyapanda780@gmail.com>
2024-07-31 12:35:20 +0530

    change

--- basics/Area.java
+++ basics/Area.java
@@ -4,6 +4,7 @@ public class Area {
     public static void main(String args[])
     {
         // area of circle is 3.14*r*r
+        // ara
         Scanner s =new Scanner (System.in);
         float r =s.nextFloat();
         //we have to use f in the side of decimal number to describe as a float
